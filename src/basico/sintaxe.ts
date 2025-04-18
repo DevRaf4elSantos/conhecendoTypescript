df6301348e64118eff9ec5f42ccd91377f1548f1
DevRaf4elSantos <contatodevrafaelsantos@gmail.com>
2025-04-18 19:58:38 -0300

    Upload iniciando estudos com typescript

---
+++ src/basico/sintaxe.ts
@@ -0,0 +1,14 @@
+
+function criarObjeto(m : string, mc : string , km : number, placa : string){
+    // Crio a tipo (estrutura) do objeto.
+    let carro : {
+        modelo : string;
+        marcar : string;
+        quilometragem : number;
+        placa : string
+    }
+    
+    carro = {modelo : m , marcar: mc, placa : placa, quilometragem : km }
+    console.log(carro)
+}
+
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let alunos : String[] = [
+    'Andersom Silva', 'Felipe Delta', "Fenando Presunto"
+]
+console.log(alunos)
