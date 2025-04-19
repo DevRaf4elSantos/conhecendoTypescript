ecdb0f39a9c6d67235676053f7af6566d3b77896
DevRaf4elSantos <contatodevrafaelsantos@gmail.com>
2025-04-18 21:01:48 -0300

    conhecendo mais sobre transpilação

---
+++ build/basico/sintaxe.js
@@ -0,0 +1,13 @@
+// Objetos e Funções: 
+function criarObjeto(m, mc, km, placa) {
+    // Crio a tipo (estrutura) do objeto.
+    let carro;
+    carro = { modelo: m, marcar: mc, placa: placa, quilometragem: km };
+    console.log(carro);
+}
+// Arrays
+let alunos = [
+    'Andersom Silva', 'Felipe Delta', "Fenando Presunto"
+];
+console.log(alunos);
+export {};
