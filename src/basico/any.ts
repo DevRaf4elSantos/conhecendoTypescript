14773196ce601d5abd38378b8665edf95e5221ad
DevRaf4elSantos <contatodevrafaelsantos@gmail.com>
2025-04-20 14:12:35 -0300

    conhecendo o tipo any

--- src/basico/any.ts
+++ src/basico/any.ts
@@ -1,15 +1,18 @@
 // Sintaxe básica
-let t1 = 1234;
-let t2 = ' sdasd asdasdas';
-let c = t1 + t2;
+let t1 : any = 1234
+let t2 : any = ' sdasd asdasdas'
+let c = t1 + t2
+
 console.log(c);
+
 // Quando any é inferido automaticamente
-let frase;
-frase = 'Olá, tudo certo?';
+let frase
+frase = 'Olá, tudo certo?'
 console.log(frase);
+
 // Forma correta de usar
-let numero;
-numero = 85;
+let numero : number
+numero = 85
 console.log(numero);
-export {};
+
 // Quando usar o ANY 
