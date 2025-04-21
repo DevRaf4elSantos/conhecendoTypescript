ef68f012cb84878b2dbc400845fe301371f2efc1
DevRaf4elSantos <contatodevrafaelsantos@gmail.com>
2025-04-21 12:29:29 -0300

    Conhencendo TypeParse e Assertion

---
+++ build/basico/assertionAndTypeCast.js
@@ -0,0 +1,12 @@
+let uValor;
+let sValor;
+let nValor;
+uValor = 200;
+nValor = uValor;
+console.log(nValor);
+sValor = '50';
+nValor = Number.parseInt(sValor);
+sValor = nValor.toString();
+console.log(nValor);
+console.log(sValor);
+export {};
